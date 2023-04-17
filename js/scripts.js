e1fdfb7fd6d87c0c7b44884821bf84f392295bc2
Milan Raut <rttmilan76@gmail.com>
2023-04-17 20:50:26 +0545

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,179 @@
- window.addEventListener('DOMContentLoaded', event => {
+ var icon = document.getElementById("theme-icon");
+var box = document.getElementById("box");
+var facebook = document.getElementById("facebook");
+var twitter = document.getElementById("twitter");
+var github = document.getElementById("github");
+var youtube = document.getElementById("youtube");
+var link1 = document.getElementById("icon-link1");
+var link2 = document.getElementById("icon-link2");
+var link4 = document.getElementById("icon-link4");
+var link5 = document.getElementById("icon-link5");
+if(localStorage.getItem("theme")=="null"){
+    localStorage.setItem("theme","dark");
+}
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
+let localData = localStorage.getItem("theme");
 
-    };
-
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 72,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
+if(localData=="light"){
+    icon.src="../assets/icons/moon.png";
+    icon.style.backgroundColor="rgba(17, 25, 235, 0.4)";
+    box.style.backgroundColor="rgba(17, 25, 235, 0.167)";
+    facebook.style.color="#3B5999";
+    twitter.style.color="#46C1F6";
+    github.style.color="#333";
+    youtube.style.color="#DE463B";
+    //facebook-hover
+    link1.addEventListener('mouseover', function handleMouseOver() {
+        facebook.style.color="white";
     });
+    link1.addEventListener('mouseout', function handleMouseOut() {
+        facebook.style.color="#3B5999";
+    });
+    //twitter-hover
+    link2.addEventListener('mouseover', function handleMouseOver() {
+        twitter.style.color="white";
+    });
+    link2.addEventListener('mouseout', function handleMouseOut() {
+        twitter.style.color="#46C1F6";
+    });
+    //github-hover
+    link4.addEventListener('mouseover', function handleMouseOver() {
+        github.style.color="white";
+    });
+    link4.addEventListener('mouseout', function handleMouseOut() {
+        github.style.color="#333";
+    });
+    //youtube-hover
+    link5.addEventListener('mouseover', function handleMouseOver() {
+        youtube.style.color="white";
+    });
+    link5.addEventListener('mouseout', function handleMouseOut() {
+        youtube.style.color="#DE463B";
+    });
+    document.body.style.backgroundColor="#5261b4d2";
+    document.body.style.backgroundImage = "url('../assets/img/back.jpg')";
+    document.body.classList.add("light-theme");
+}
+else if(localData=="dark"){
+    icon.src="../assets/icons/sun.png";
+    icon.style.backgroundColor="rgba(0,0,0,0.6)";
+    box.style.backgroundColor="rgba(0, 0, 0, 0.6)";
+    facebook.style.color="black";
+    twitter.style.color="black";
+    github.style.color="black";
+    youtube.style.color="black";
+    //facebook-hover
+    link1.addEventListener('mouseover', function handleMouseOver() {
+        facebook.style.color="white";
+    });
+    link1.addEventListener('mouseout', function handleMouseOut() {
+        facebook.style.color="black";
+    });
+    //twitter-hover
+    link2.addEventListener('mouseover', function handleMouseOver() {
+        twitter.style.color="white";
+    });
+    link2.addEventListener('mouseout', function handleMouseOut() {
+        twitter.style.color="black";
+    });
+    //github-hover
+    link4.addEventListener('mouseover', function handleMouseOver() {
+        github.style.color="white";
+    });
+    link4.addEventListener('mouseout', function handleMouseOut() {
+        github.style.color="black";
+    });
+    //youtube-hover
+    link5.addEventListener('mouseover', function handleMouseOver() {
+        youtube.style.color="white";
+    });
+    link5.addEventListener('mouseout', function handleMouseOut() {
+        youtube.style.color="black";
+    });
+    document.body.style.backgroundColor="#333";
+    document.body.style.backgroundImage = "url('../assets/img/background.jpg')";
+    document.body.classList.remove("light-theme");
+}
 
-});
+icon.onclick = function(){
+    document.body.classList.toggle("light-theme");
+    if(document.body.classList.contains("light-theme")){
+        icon.src="../assets/icons/moon.png";
+        icon.style.backgroundColor="rgba(17, 25, 235, 0.4)";
+        box.style.backgroundColor="rgba(17, 25, 235, 0.167)";
+        facebook.style.color="#3B5999";
+        twitter.style.color="#46C1F6";
+        github.style.color="#333";
+        youtube.style.color="#DE463B";
+        //facebook-hover
+        link1.addEventListener('mouseover', function handleMouseOver() {
+            facebook.style.color="white";
+        });
+        link1.addEventListener('mouseout', function handleMouseOut() {
+            facebook.style.color="#3B5999";
+        });
+        //twitter-hover
+        link2.addEventListener('mouseover', function handleMouseOver() {
+            twitter.style.color="white";
+        });
+        link2.addEventListener('mouseout', function handleMouseOut() {
+            twitter.style.color="#46C1F6";
+        });
+        //github-hover
+        link4.addEventListener('mouseover', function handleMouseOver() {
+            github.style.color="white";
+        });
+        link4.addEventListener('mouseout', function handleMouseOut() {
+            github.style.color="#333";
+        });
+        //youtube-hover
+        link5.addEventListener('mouseover', function handleMouseOver() {
+            youtube.style.color="white";
+        });
+        link5.addEventListener('mouseout', function handleMouseOut() {
+            youtube.style.color="#DE463B";
+        });
+        document.body.style.backgroundColor="#5261b4d2";
+        document.body.style.backgroundImage = "url('../assets/img/back.jpg')";
+        localStorage.setItem("theme","light");
+    }else{
+        icon.src="../assets/icons/sun.png";
+        icon.style.backgroundColor="rgba(0,0,0,0.6)";
+        box.style.backgroundColor="rgba(0, 0, 0, 0.6)";
+        facebook.style.color="black";
+        twitter.style.color="black";
+        github.style.color="black";
+        youtube.style.color="black";
+        //facebook-hover
+        link1.addEventListener('mouseover', function handleMouseOver() {
+            facebook.style.color="white";
+        });
+        link1.addEventListener('mouseout', function handleMouseOut() {
+            facebook.style.color="black";
+        });
+        //twitter-hover
+        link2.addEventListener('mouseover', function handleMouseOver() {
+            twitter.style.color="white";
+        });
+        link2.addEventListener('mouseout', function handleMouseOut() {
+            twitter.style.color="black";
+        });
+        //github-hover
+        link4.addEventListener('mouseover', function handleMouseOver() {
+            github.style.color="white";
+        });
+        link4.addEventListener('mouseout', function handleMouseOut() {
+            github.style.color="black";
+        });
+        //youtube-hover
+        link5.addEventListener('mouseover', function handleMouseOver() {
+            youtube.style.color="white";
+        });
+        link5.addEventListener('mouseout', function handleMouseOut() {
+            youtube.style.color="black";
+        });
+        document.body.style.backgroundColor="#333";
+        document.body.style.backgroundImage = "url('../assets/img/background.jpg')";
+        localStorage.setItem("theme","dark");
+    }
+};
